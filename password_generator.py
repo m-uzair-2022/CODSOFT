8256dafa84edc7293ce1614b93e4039015477f09
M.Uzair Ansar <aansari3028@gmail.com>
2026-02-01 23:57:29 +0530

    Task 3 Updated

--- password_generator.py
+++ password_generator.py
@@ -7,19 +7,25 @@ print("_______________________________________")
 print("          PASSWORD GENERATOR           ")
 print("_______________________________________\n")
 
-try:
-    length = int(input("Enter Password length: "))
-except:
-    print("Please! Enter only float numbers.")
-    exit()
+while(True):
+    try:
+        length = int(input("Enter Password length (0 = Cancel): "))
+        if length == 0 :
+            print("\n_______________________________________")
+            print("            THANKS FOR COMING!           ")
+            print("\n_______________________________________")
+            break
+    except:
+        print("Please! Enter only integer numbers.")
+        exit()
 
-characters = string.ascii_letters + string.digits + string.punctuation
+    characters = string.ascii_letters + string.digits + string.punctuation
 
-password = ""
+    password = ""
 
-for i in range(length):
-    password += random.choice(characters)
+    for i in range(length):
+        password += random.choice(characters)
 
-print("Generated Password: ", password)
+    print("Generated Password: ", password)
 
-print("\n_______________________________________")
+    print("\n_______________________________________")
